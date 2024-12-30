a727dcbe92169538361ef31578e81a2c87998b4d
Dilhan.D <dilhan.dincer@epitech.eu>
2024-12-31 00:25:07 +0100

    form name & age

--- cobol.cbl
+++ cobol.cbl
@@ -1,11 +1,26 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. HelloWorld.
-
-       ENVIRONMENT DIVISION.
+       PROGRAM-ID. cobol.
 
        DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-NAME     PIC X(10).
+       01 WS-AGE      PIC 9(3).
 
        PROCEDURE DIVISION.
        BEGIN.
-           DISPLAY "Hello, World!".
+           DISPLAY "Enter your name: " WITH NO ADVANCING.
+           ACCEPT WS-NAME.
+
+           DISPLAY "Hello, " WS-NAME "! Welcome to COBOL programming!".
+           DISPLAY "Enter your age: " WITH NO ADVANCING.
+           ACCEPT WS-AGE.
+
+           IF WS-AGE < 30
+               DISPLAY "It'ss fantastic to see young talent "
+                       "investing time in IBM technologies!"
+           ELSE
+               DISPLAY "Your experience will be a great asset "
+                       "in mastering IBM environments."
+           END-IF.
+
            STOP RUN.
