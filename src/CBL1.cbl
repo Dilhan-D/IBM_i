f2c413ac8104f5dc550bc8e0c54cfb5091400e2d
Dilhan D <dilhan.dincer@epitech.eu>
2025-12-28 23:44:37 +0100

    Parent variables and comments

---
+++ src/CBL1.cbl
@@ -0,0 +1,34 @@
+      * 
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CBL1.
+       AUTHOR. Dilhan.
+       DATE-WRITTEN. 2025-12-28
+
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+      *FILE SECTION.                    *>  FOR EXTERNAL FILES
+       WORKING-STORAGE SECTION. 
+         01  PERSON.
+           05 V-NAME      PIC A(20).    *> Alphanumeric field for name
+           05 V-AGE       PIC 9(5).     *> Numeric field for age
+           05 V-ADDRESS.                *> Pas de PIC pour les FORMATS   
+              10 V-STREET PIC A(30).
+              10 V-CITY   PIC X(20).
+              10 V-STATE  PIC 9(3).
+       PROCEDURE DIVISION.
+       BEGIN.
+           MOVE "Dilhan" TO V-NAME.
+           MOVE 28 TO V-AGE.
+           MOVE "Portsmouth, VA 23704, États-Unis"
+           TO V-ADDRESS.
+           MOVE "2211 Frederick Blvd" TO V-STREET.
+           MOVE "Norfolk" TO V-CITY.
+           MOVE +11111 TO V-STATE.  
+           DISPLAY "Age =" V-AGE.
+           DISPLAY "Name =" V-NAME.
+      *    DISPLAY "Address =" V-ADDRESS.
+      *    DISPLAY "Street =" V-STREET.
+      *    DISPLAY "City =" V-CITY.
+      *    DISPLAY "State =" V-STATE.
+           DISPLAY V-ADDRESS " " V-STREET " " V-CITY " " V-STATE.
+           STOP RUN.
