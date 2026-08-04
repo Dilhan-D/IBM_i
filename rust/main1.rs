6ed449ed34ed152050c9e444a383df37ba5a46d9
Dilhan_d <dilhan.dincer@epitech.eu>
2026-08-04 14:28:57 +0200

    We continue the exercises in rust

--- rust/main1.rs
+++ rust/main1.rs
@@ -2,6 +2,12 @@
 // @date 2026-05-28
  
 fn main() {
+  let name = "Diana";
+  let age = 30;
+  println!("Hello!", name);
+  println!("His name is {} and he is {} years old.", name, age);
+  age = 31;
+  println!("His name is {} and now he is {} years old.", name, age);
   println!("Hello World!");
   println!("I am \n \nlearning Rust.");
   println!("\n");
